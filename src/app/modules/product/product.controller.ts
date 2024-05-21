a1098593a2154fe42d99eefe38a5741fa0741b68
shuvo-99 <anontoshuvo99@gmail.com>
2024-05-22 01:00:13 +0600

    updated

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -3,7 +3,7 @@ import { ProductServices } from "./product.service";
 
 const createProduct = async (req: Request, res: Response) => {
   try {
-    const { product: productData } = req.body;
+    const productData = req.body;
 
     const result = await ProductServices.createProductIntoDB(productData);
 
